8c7a195fd42b3b4e011f2c6b1468768f39c6d6a7
vanjaw <vanja.wallner@me.com>
2019-11-29 13:06:32 +0100

    can add groups/lists/tasks

--- client/AuthLoadingScreen.js
+++ client/AuthLoadingScreen.js
@@ -21,7 +21,7 @@ export default class AuthLoadingScreen extends React.Component {
 
     // This will switch to the App screen or Auth screen and this loading
     // screen will be unmounted and thrown away.
-    this.props.navigation.navigate(userToken ? ("App", {addButton: null}) : "Auth");
+    this.props.navigation.navigate(userToken ? ("App", { addButton: null }) : "Auth");
   };
 
   // Render any loading content that you like here

6473f57d2653c34067ac892733614790293518c6
Axel <axel949@hotmail.com>
2019-12-17 12:05:53 +0100

    blablabla

---
+++ client/__tests__/clienttest.js
@@ -0,0 +1,11 @@
+// __tests__/Intro-test.js
+import React from 'react';
+import GroupsScreen from '../GroupsScreen';
+
+import renderer from 'react-test-renderer';
+
+test('renders correctly', () => {
+  // const tree = renderer.create(<GroupsScreen />).toJSON();
+  // expect(tree).toMatchSnapshot();
+  console.log("1");
+});
